a14e0223e34fbbc453687550363ec3616bdff8fd
raoakif <akifrao@gmail.com>
2022-10-09 04:21:08 +0500

    Add About and Contact Pages

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/About.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>About Page</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Contact.module.css'
+
+const Contact: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>CONTACT PAGE</h1>
+    </div>
+  )
+}
+
+export default Contact
